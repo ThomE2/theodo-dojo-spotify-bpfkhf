0454761a89bad1de9b9797b843c2b81e6a15cc54
ThomE2 <67593304+ThomE2@users.noreply.github.com>
2023-04-08 10:00:56 +0200

    feat: title and homepage text

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test</h1>
+        <h1 className="App-title">Le merveilleux BlindTest Theodo</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>Vous êtes prêts ?</p>
       </div>
       <div className="App-buttons"></div>
     </div>
